ad587ac25d3b90edbb40b5cf83cbfc81c6642755
Miguel Bernard <mig@miguelbernard.com>
2025-11-13 17:37:27 -0500

    Add RevoTT API endpoint to the configuration

--- cross-seed/config.js
+++ cross-seed/config.js
@@ -24,6 +24,7 @@ module.exports = {
         "http://prowlarr.prowlarr/11/api?apikey=<path:azurekv-secrets#prowlarr-api-key>&extended=1&t=search", // Seedpool
         "http://prowlarr.prowlarr/12/api?apikey=<path:azurekv-secrets#prowlarr-api-key>&extended=1&t=search", // ABTorrents
         "http://prowlarr.prowlarr/13/api?apikey=<path:azurekv-secrets#prowlarr-api-key>&extended=1&t=search", // Digital core
+        "http://prowlarr.prowlarr/14/api?apikey=<path:azurekv-secrets#prowlarr-api-key>&extended=1&t=search", // RevoTT
     ],
 
     sonarr: ["http://sonarr.sonarr?apikey=<path:azurekv-secrets#sonarr-api-key>"],
